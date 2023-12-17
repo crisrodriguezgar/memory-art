539f682e1fee0d987ae3bfe52abd1e18577634c1
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-12-17 01:23:54 +0100

    added new styles and photos

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 
 const cards = document.querySelectorAll('.js-card');
 const defaultImages = document.querySelectorAll('.js-default');
@@ -9,7 +9,8 @@ const btnReset = document.querySelector('.js-btn');
 let flippedCards = [];
 let lockBoard = false;
 
-function flipCard(cardElement) { // Función para voltear una tarjeta cuando se hace clic en ella.
+function flipCard(cardElement) {
+  // Función para voltear una tarjeta cuando se hace clic en ella.
   if (lockBoard) return; // Si el tablero está bloqueado, no hace nada.
   if (cardElement === flippedCards[0]) return; // Si la tarjeta ya está volteada, no hace nada.
 
@@ -19,58 +20,62 @@ function flipCard(cardElement) { // Función para voltear una tarjeta cuando se 
   defaultImg.classList.add('hidden');
   artImg.classList.remove('hidden');
 
-  if (flippedCards.length === 0) { // Si no hay tarjetas volteadas, agrega la actual a la lista.
+  if (flippedCards.length === 0) {
+    // Si no hay tarjetas volteadas, agrega la actual a la lista.
     flippedCards.push(cardElement);
     return;
-  } else { // Si ya hay una tarjeta volteada, compara las imágenes.
+  } else {
+    // Si ya hay una tarjeta volteada, compara las imágenes.
     flippedCards.push(cardElement);
 
     const firstCardId = flippedCards[0].querySelector('.js-img').id;
     const secondCardId = flippedCards[1].querySelector('.js-img').id;
 
-    if (firstCardId === secondCardId) { // Si las imágenes coinciden, vacía la lista de tarjetas volteadas.
+    if (firstCardId === secondCardId) {
+      // Si las imágenes coinciden, vacía la lista de tarjetas volteadas.
       flippedCards = [];
-    } else { // Si las imágenes no coinciden, se vuelven a voltear después de un breve tiempo.
+    } else {
+      // Si las imágenes no coinciden, se vuelven a voltear después de un breve tiempo.
       lockBoard = true; // Bloquea el tablero.
-      setTimeout(() => { 
-        flippedCards.forEach(card => {
+      setTimeout(() => {
+        flippedCards.forEach((card) => {
           card.querySelector('.js-default').classList.remove('hidden');
           card.querySelector('.js-img').classList.add('hidden');
         });
         flippedCards = []; // Vacía la lista de tarjetas volteadas.
         lockBoard = false; // Desbloquea el tablero para continuar el juego.
-      }, 1000); 
+      }, 1000);
     }
   }
 }
 
-function hideHiddenImages() { 
-  artImages.forEach(img => {
+function hideHiddenImages() {
+  artImages.forEach((img) => {
     img.classList.add('hidden');
   });
 }
 
 function setup() {
   hideHiddenImages();
-
-  cards.forEach(card => {
-    card.addEventListener('click', function() {flipCard(card);});
+  cards.forEach((card) => {
+    card.addEventListener('click', function () {
+      flipCard(card);
+    });
   });
 }
 
 setup();
 
+function handleReset() {
+  hideHiddenImages();
 
-function handleReset(){
-    hideHiddenImages();
-  
-    cards.forEach(card => {
-      card.querySelector('.js-default').classList.remove('hidden');
-      card.querySelector('.js-img').classList.add('hidden');
-    });
-  
-    flippedCards = [];
-    lockBoard = false;
-  }
+  cards.forEach((card) => {
+    card.querySelector('.js-default').classList.remove('hidden');
+    card.querySelector('.js-img').classList.add('hidden');
+  });
+
+  flippedCards = [];
+  lockBoard = false;
+}
 
 btnReset.addEventListener('click', handleReset);
