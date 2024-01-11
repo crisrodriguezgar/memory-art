68552ce86ecbf320d24f69814489c974429bee1c
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2024-01-11 19:05:52 +0100

    fixing errors

--- src/js/main.js
+++ src/js/main.js
@@ -37,6 +37,27 @@ const countTime = () => {
   }, 1000);
 };
 
+const imagePaths = [
+  './assets/images/frame.png',
+  './assets/images/1.jpg',
+  './assets/images/2.jpg',
+  './assets/images/3.jpg',
+  './assets/images/4.jpg',
+  './assets/images/5.jpg',
+  './assets/images/6.jpg',
+  './assets/images/7.jpg',
+  './assets/images/8.jpg',
+];
+
+const preloadImages = (paths) => {
+  paths.forEach((path) => {
+    const img = new Image();
+    img.src = path;
+  });
+};
+
+preloadImages(imagePaths);
+
 const resetAllCards = () => {
   allCards.forEach((card) => {
     card.innerHTML = '';
